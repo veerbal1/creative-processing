311b8c3a74d7d8105434f3b2db6d0328df927a4c
veerbal1 <veerbal1singh1@gmail.com>
2022-11-17 15:42:22 +0530

    Detected keywords

--- components/Creative.js
+++ components/Creative.js
@@ -6,8 +6,25 @@ const Creative = () => {
     const file = e.target.files[0];
     JSZip.loadAsync(file).then((zip) => {
       Object.keys(zip.files).forEach((filename) => {
-        let content = zip.files[filename];
-        console.log(filename, content);
+        if (filename === 'Pro_Fight_League_Dynamic_v02_.js') {
+          zip.files[filename].async('string').then((data) => {
+            let keyword = 'new cjs.Text(';
+            // Check indexs of keyword in data
+            let multipleIndex = [];
+            let index = data.indexOf(keyword);
+            while (index >= 0) {
+              multipleIndex.push(index);
+              index = data.indexOf(keyword, index + 1);
+            }
+            // console.log(multipleIndex);
+            multipleIndex.forEach((index) => {
+              let start = index + keyword.length;
+              let end = data.indexOf(',', start);
+              let text = data.substring(start, end);
+              console.log(start, end, text);
+            });
+          });
+        }
       });
     });
   };
