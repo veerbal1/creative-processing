ba03cf365f3de462acc8f87aa705b90aaaad8e6c
veerbal1 <veerbal1singh1@gmail.com>
2022-11-17 17:17:14 +0530

    replacing text

--- components/Creative.js
+++ components/Creative.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import JSZip from 'jszip';
+import { scripts } from './scripts';
 
 const Creative = () => {
   const handleSelect = (e) => {
@@ -8,6 +9,25 @@ const Creative = () => {
       Object.keys(zip.files).forEach((filename) => {
         if (filename === 'Pro_Fight_League_Dynamic_v02_.js') {
           zip.files[filename].async('string').then((data) => {
+            // Update script Date
+            const script = scripts['day-hour-min'];
+            const scriptDate = script.match(/new Date\((.*?)\)/)[1];
+            // it is in 2022,10,23,20,0,0 format
+            let date = new Date('2022-11-30T11:13:06.145Z');
+            // convert to 2022,10,23,20,0,0 format
+            date = dateFormat(date);
+            console.log(date);
+            const updatedScript = script.replace(scriptDate, date);
+            // For adding countdown function code
+            // insert string after "{"
+            const indexOfFunc = data.indexOf('{');
+            const newString =
+              data.slice(0, indexOfFunc + 1) +
+              updatedScript +
+              data.slice(indexOfFunc + 1);
+            data = newString;
+            // console.log(newString);
+            // For replace dynamic values
             let keyword = 'new cjs.Text(';
             // Check indexs of keyword in data
             let multipleIndex = [];
@@ -23,23 +43,31 @@ const Creative = () => {
               let end = data.indexOf(',', start);
               let text = data.substring(start, end);
               spottedKeywords.push(text);
-              console.log(data.substring(start - 5, end + 5));
             });
-
-            console.log('spottedKeywords', spottedKeywords);
             // Replace here
             spottedKeywords.forEach((keyword) => {
               let index = data.indexOf(keyword);
               let start = index;
               let end = index + keyword.length;
               let text = data.substring(start, end);
-              let replaced = text.replace(
-                keyword,
-                'Math.floor(Math.random() * 100)'
-              );
-              data = data.replace(text, replaced);
+              console.log(text);
+
+              if (text === '"24"') {
+                let replaced = text.replace(keyword, 'leftDays.seconds');
+                data = data.replace(text, replaced);
+              } else if (text === '"16"') {
+                let replaced = text.replace(keyword, 'leftDays.minutes');
+                data = data.replace(text, replaced);
+              } else if (text === '"8"') {
+                let replaced = text.replace(keyword, 'leftDays.hours');
+                data = data.replace(text, replaced);
+              }
             });
-            console.log('data', data);
+            // Save file
+            let updatedZip = zip.file(filename, data);
+            updatedZip.generateAsync({ type: 'blob' }).then((content) => {
+              saveAs(content, 'updated.zip');
+            });
           });
         }
       });
@@ -48,4 +76,27 @@ const Creative = () => {
   return <input type="file" onChange={handleSelect} />;
 };
 
+const saveAs = (blob, fileName) => {
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.download = fileName;
+  link.click();
+};
+
+const dateFormat = (date) => {
+  // convert to 2022,10,23,20,0,0 format
+  return (
+    date.getFullYear() +
+    ',' +
+    (date.getMonth() <= 9 ? '0' + date.getMonth() : date.getMonth()) +
+    ',' +
+    (date.getDate() <= 9 ? '0' + date.getDate() : date.getDate()) +
+    ',' +
+    (date.getHours() <= 9 ? '0' + date.getHours() : date.getHours()) +
+    ',' +
+    (date.getMinutes() <= 9 ? '0' + date.getMinutes() : date.getMinutes()) +
+    ',' +
+    (date.getSeconds() <= 9 ? '0' + date.getSeconds() : date.getSeconds())
+  );
+};
 export default Creative;
---
+++ components/scripts.js
@@ -0,0 +1,5 @@
+export const scripts = {
+  'hour-min-sec': null,
+  'day-hour-min':
+    'const stopDateTime=new Date(2022,10,23,20,0,0);function convertTimeZone(e,t){return new Date(("string"==typeof e?new Date(e):e).toLocaleString("en-US",{timeZone:t}))}function getLeftDays(e,t){e=Math.max(t-e,0);return{days:Math.floor(e/864e5),hours:Math.floor(e/36e5%24),minutes:Math.floor(e/6e4%60),seconds:Math.floor(e/1e3%60)}}function convertTimeZone(e,t){return new Date(("string"==typeof e?new Date(e):e).toLocaleString("en-US",{timeZone:t}))}console.log(stopDateTime);var currentDateTimeNY=convertTimeZone(new Date,"America/New_York"),leftDays=getLeftDays(currentDateTimeNY,stopDateTime);',
+};
